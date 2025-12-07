0d956eeb22fea1cb5d503da800deb5024ac911a6
JoaoCarlosForment0 <jcformento13@gmail.com>
2025-12-07 20:01:48 -0300

    cors ainda nao curtiu, então segue mais uma tentativa de resolver ele aceitar qualquer url do vercel.app

--- src/app.js
+++ src/app.js
@@ -11,13 +11,22 @@ import usuariosRoutes from "./routes/usuarios.routes.js";
 import homeRoutes from "./routes/home.routes.js";
 
 const app = express();
-app.use(cors({ origin: 'https://help-desk-front-blush.vercel.app' }));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (/\.vercel\.app$/.test(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error("Not allowed by CORS"));
+    },
+  })
+);
 app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/chamados", chamadosRoutes);
-app.use('/usuarios', usuariosRoutes);
-app.use('/home', homeRoutes)
+app.use("/usuarios", usuariosRoutes);
+app.use("/home", homeRoutes);
 
 const PORT = process.env.PORT || 3000; // Adiciona um fallback para a porta
 
@@ -27,7 +36,9 @@ app.listen(PORT, async () => {
     await database.sync();
     console.log(`Servidor  na porta ${PORT}`);
   } catch (error) {
-    console.error('Erro ao iniciar o servidor ou sincronizar as tabelas:', error);
+    console.error(
+      "Erro ao iniciar o servidor ou sincronizar as tabelas:",
+      error
+    );
   }
 });
-
